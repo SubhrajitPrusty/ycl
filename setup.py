101c3107dab9ec112c232e792ccf9515d6838355
Subhrajit Prusty <subhrajit1997@gmail.com>
2019-03-26 22:58:45 +0530

    added setup.py

---
+++ setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+def readme():
+    try:
+        with open('README.md') as f:
+            return f.read()
+    except:
+        pass
+
+setup(name = 'ycl',
+    version = '1.0',
+    description = 'youtube control via command line',
+    long_description=readme(),  
+    long_description_content_type='text/markdown', 
+
+    author='Subhrajit Prusty',  
+    author_email='subhrajit1997@gmail.com',
+    url = 'http://github.com/SubhrajitPrusty/ycl',
+
+    setup_requires=['setuptools>=40.0.0'],
+    
+    classifiers=[  
+        'Development Status :: 4 - Beta',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    license = 'MIT',
+    packages=find_packages(), 
+    install_requires=['python-dotenv', 'requests', 'youtube-dl', 'pick', 'windows-curses'],
+    entry_points="""
+    [console_scripts]
+        wallgen=wallgen:cli
+        """,
+)
