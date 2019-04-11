d18e2cb925fbe56a5e0eb703fb993a35a6bd9c0a
Subhrajit Prusty <subhrajit1997@gmail.com>
2019-04-11 21:44:21 +0530

    fixed setup.py syntax for os specific builds

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(name = 'ycl',
 
     license = 'MIT',
     packages=find_packages(), 
-    install_requires=['python-dotenv', 'requests', 'youtube-dl', 'pick', 'Click'] + ['windows-curses'] if "win" in sys.platform else [],
+    install_requires=['python-dotenv', 'requests', 'youtube-dl', 'pick', 'Click', 'windows-curses; platform_system == "Windows"'],
     entry_points="""
     [console_scripts]
         ycl=ycl:cli
