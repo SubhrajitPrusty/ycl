f77b8ff69fcf5b7796dceccea9f90d58d6f1c9e3
Subhrajit <subhrajit1997@gmail.com>
2019-06-07 21:42:01 +0530

    added interactive option in cli

--- ycl.py
+++ ycl.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import tui
 import click
 from tools.player import *
 from tools.youtube import *
@@ -22,91 +23,95 @@ except Exception as e:
 @click.option("--playlistsearch", "-ps", default=False, is_flag=True, help="Searches for playlists")
 @click.option("--video", "-v", default=False, is_flag=True, help="Use a direct video link")
 @click.option("--playlist", "-pl", default=False, is_flag=True, help="Use a direct playlist link")
+@click.option("--interactive", "-i", default=False, is_flag=True, help="Starts an interactive session")
 
-def cli(query, playlistsearch, video, playlist):
-	if not query:
-		print("Error: Enter a search query")
-		sys.exit(1)
-
-	query = " ".join(query)
-
-	url = ""
-	choice = dict()
-
-	if video:
-		isValid, details = isValidURL(query, urlType="video")
-		if isValid:
-			# print(f"Selected : {url}")
-			choice['id'] = details['id']
-			choice['url'] = query
-			choice['title'] = details['snippet']['title']
-		else:
-			print("Invalid URL")
-			sys.exit(3)
-	elif playlist:
-		isValid, details = isValidURL(query, urlType="playlist")
-		url = query
-		if isValid:
-			# print(f"Selected : {url}")
-			choice['id'] = details['id']
-			choice['url'] = query
-			choice['title'] = details['snippet']['title']
-		else:
-			print("Invalid URL")
-			sys.exit(3)
+def cli(query, playlistsearch, video, playlist, interactive):
+	if interactive:
+		tui.main()
 	else:
-		if playlistsearch:
-			results = search_pl(query)
+		if not query:
+			print("Error: Enter a search query")
+			sys.exit(1)
+
+		query = " ".join(query)
+
+		url = ""
+		choice = dict()
+
+		if video:
+			isValid, details = isValidURL(query, urlType="video")
+			if isValid:
+				# print(f"Selected : {url}")
+				choice['id'] = details['id']
+				choice['url'] = query
+				choice['title'] = details['snippet']['title']
+			else:
+				print("Invalid URL")
+				sys.exit(3)
+		elif playlist:
+			isValid, details = isValidURL(query, urlType="playlist")
+			url = query
+			if isValid:
+				# print(f"Selected : {url}")
+				choice['id'] = details['id']
+				choice['url'] = query
+				choice['title'] = details['snippet']['title']
+			else:
+				print("Invalid URL")
+				sys.exit(3)
 		else:
-			results = search_video(query)
+			if playlistsearch:
+				results = search_pl(query)
+			else:
+				results = search_video(query)
 
-		if len(results) < 1:
-			click.secho("No results found.", fg="red")
-			sys.exit(3)
+			if len(results) < 1:
+				click.secho("No results found.", fg="red")
+				sys.exit(3)
 
-		options = [x["title"] for x in results]
-		title = f"Search results for {query} (q to quit)"
+			options = [x["title"] for x in results]
+			title = f"Search results for {query} (q to quit)"
 
+			picker = Picker(options, title)
+			picker.register_custom_handler(ord('q'), quit_pick)
+			option, index = picker.start()
+
+			choice = results[index]
+			
+		print(f"Selected : {choice['url']}")
+		
+		options = ["Play", "Download"]
+		title = "Choose what you want to do (q to quit)"
 		picker = Picker(options, title)
+
 		picker.register_custom_handler(ord('q'), quit_pick)
+
 		option, index = picker.start()
-
-		choice = results[index]
-		
-	print(f"Selected : {choice['url']}")
-	
-	options = ["Play", "Download"]
-	title = "Choose what you want to do (q to quit)"
-	picker = Picker(options, title)
-
-	picker.register_custom_handler(ord('q'), quit_pick)
-
-	option, index = picker.start()
-		
-	if playlist or playlistsearch:
-		if option == "Download":
-			for playlist_item in extract_playlist_data(choice['url']):
-				print(f"Downloading {playlist_item['title']}")
-				download_video(playlist_item['url'], print_hook)
-		elif option == "Play":
-			if not PLAY_SUPPORT:
-				print("Play support is not available for your system.")
-				sys.exit(2)
-			else:
+			
+		if playlist or playlistsearch:
+			if option == "Download":
 				for playlist_item in extract_playlist_data(choice['url']):
-					print(f"Playing {playlist_item['title']}")
-					play_audio(playlist_item['url'])
-	else:
-		if option == "Download":
-				print(f"Dowloading {choice['title']}")
-				download_video(choice['url'], print_hook)
-		elif option == "Play":
-			if not PLAY_SUPPORT:
-				print("Play support is not available for your system.")
-				sys.exit(2)
-			else:
-				print(f"Playing {choice['title']}")
-				play_audio(choice['url'])
+					print(f"Downloading {playlist_item['title']}")
+					download_video(playlist_item['url'], print_hook)
+			elif option == "Play":
+				if not PLAY_SUPPORT:
+					print("Play support is not available for your system.")
+					sys.exit(2)
+				else:
+					for playlist_item in extract_playlist_data(choice['url']):
+						print(f"Playing {playlist_item['title']}")
+						play_audio(playlist_item['url'])
+		else:
+			if option == "Download":
+					print(f"Dowloading {choice['title']}")
+					download_video(choice['url'], print_hook)
+			elif option == "Play":
+				if not PLAY_SUPPORT:
+					print("Play support is not available for your system.")
+					sys.exit(2)
+				else:
+					print(f"Playing {choice['title']}")
+					play_audio(choice['url'])
 
 
 if __name__ == '__main__':
