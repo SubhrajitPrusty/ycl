ed4c4fc0585eb3d220ed2c1262cb378814361526
Subhrajit <subhrajit.prusty@racetrack.ai>
2020-01-10 14:43:07 +0530

    code reformating
    * divided code into functions so that tui loops after wrong query input
    
    * renamed function to distinguish between imported function of same name
    
    * reversed playlist search options, keeping in mind primary usecase

--- tools/tui.py
+++ tools/tui.py
@@ -53,7 +53,7 @@ class Window(object):
 		sys.exit(0)
 
 LOOP=True
-def play_audio(screen, url, title=None):
+def play_audio_tui(screen, url, title=None):
 	stdscr = screen.stdscr
 	stdscr.nodelay(1)
 	player = create_player(url)
@@ -69,7 +69,7 @@ def play_audio(screen, url, title=None):
 		stdscr.refresh()
 		pos_str, pos, dur = get_player_pos(player)
 		stdscr.addstr(3,  2, f"{state} : {pos_str} \t\tVolume: {get_vol(player)}", curses.color_pair(1))
-		stdscr.hline(4,  2, curses.ACS_HLINE, curses.COLS-4)
+		stdscr.hline (4,  2, curses.ACS_HLINE, curses.COLS-4)
 		stdscr.addstr(6,  2, "CONTROLS : ", curses.color_pair(3) | curses.A_BOLD)
 		stdscr.addstr(7,  2, "s             : STOP (Start next song in playlist) ", curses.color_pair(3))
 		stdscr.addstr(8,  2, "SPACE         : Toggle PLAY/PAUSE ", curses.color_pair(3))
@@ -121,42 +121,57 @@ def play_audio(screen, url, title=None):
 
 def main():
 	scr = Window()
-	try:
-		query = scr.take_input(2, 2, "Search : ")
-		scr.reset()
-		while len(query) < 1:
+	def search_page():
+		try:
 			scr.reset()
-			scr.draw(2, 2, "You did not enter a valid query", curses.color_pair(2))
-			sleep(2)
-			scr.reset()
+			scr.draw(curses.LINES - 4, 2, "Ctrl+C to exit")
 			query = scr.take_input(2, 2, "Search : ")
+			scr.reset()
+			while len(query) < 1:
+				scr.reset()
+				scr.draw(2, 2, "You did not enter a valid query", curses.color_pair(2))
+				scr.draw(curses.LINES - 4, 2, "Ctrl+C to exit")
+				sleep(2)
+				scr.reset()
+				scr.draw(curses.LINES - 4, 2, "Ctrl+C to exit")
+				query = scr.take_input(2, 2, "Search : ")
 
-	except KeyboardInterrupt:
-		scr.quit()
+		except KeyboardInterrupt:
+			scr.quit()
+
+		return query
 			
-	title = f"Search for playlists? : (q to quit)"
-	options = ["Yes", "No"]
-	picker = Picker(options, title, indicator="=>")
-	picker.register_custom_handler(ord('q'), scr.quit)
-	option, index = picker._start(scr.stdscr)
-	scr.reset()
-	curses.curs_set(0)
-	try:
-		if index == 0:
-			results = search_pl(query)
-			playlist = True
-		else:
-			results = search_video(query)
-			playlist = False
-	except Exception as e:
-		scr.draw(2, 2, f"Oops! Make sure you are connected to the internet", curses.color_pair(2))
-		sleep(2)
-		scr.quit()
-	
-	if len(results) < 1:
+	def make_search():
+		query = search_page()
+		title = f"Search for playlists? : (q to quit)"
+		options = ["No", "Yes"]
+		picker = Picker(options, title, indicator="=>")
+		picker.register_custom_handler(ord('q'), scr.quit)
+		option, index = picker._start(scr.stdscr)
+		scr.reset()
+		curses.curs_set(0)
+		try:
+			if index == 1:
+				results = search_pl(query)
+				isPlaylist = True
+			else:
+				results = search_video(query)
+				isPlaylist = False
+		except Exception as e:
+			#scr.draw(curses.LINES - 4, 2, f"ERROR: {str(e)}")
+			scr.draw(2, 2, f"Oops! Make sure you are connected to the internet", curses.color_pair(2))
+			sleep(2)
+			scr.quit()
+
+		return results, query, isPlaylist
+		
+	results, query, isPlaylist = make_search()
+
+	while len(results) < 1:
 		scr.draw(2, 2, "No results found!", curses.color_pair(2))
-		sleep(5)
-		scr.quit()
+		sleep(2)
+		results, query = make_search()
+	
 
 	# Pick results
 	title = f"You searched for {query}. Search results: (q to quit)"
@@ -178,12 +193,12 @@ def main():
 	# Perform action
 	if index == 0:
 		try:
-			if playlist:
+			if isPlaylist:
 				for video in extract_playlist_data(choice['url']):
 					scr.reset()
-					play_audio(scr, video['url'], video['title'])
+					play_audio_tui(scr, video['url'], video['title'])
 			else:
-				play_audio(scr, choice['url'], choice['title'])
+				play_audio_tui(scr, choice['url'], choice['title'])
 		except Exception as e:
 			scr.draw(2, 2, f"Oops! Check your internet connection", curses.color_pair(2))
 			sleep(2)
@@ -191,7 +206,7 @@ def main():
 
 	else:
 		curses.endwin()
-		if playlist:
+		if isPlaylist:
 			for video in extract_playlist_data(choice['url']):
 				print(f"Downloading {video['title']}")
 				download_video(video['url'], print_hook)
